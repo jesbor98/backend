2c85d2d43ca5eec3fd4e7fb14d5bb893e7dd2aea
jesbor98 <112757660+jesbor98@users.noreply.github.com>
2022-11-11 09:23:48 +0100

    changed host/port in Client, Server in 212

--- src/uppgift212/Server.java
+++ src/uppgift212/Server.java
@@ -50,7 +50,7 @@ public class Server {
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(2000);
+        ServerSocket serverSocket = new ServerSocket(2001);
         Server server = new Server(serverSocket);
         server.startServer();
     }
