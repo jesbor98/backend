29f025a594fa6ad042219d30a040ab2d5679d654
jesbor98 <jessicalborg@gmail.com>
2023-04-27 07:44:05 +0200

    DONE!

--- src/uppgift214/Client.java
+++ src/uppgift214/Client.java
@@ -1,13 +1,6 @@
 package uppgift214;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 import javax.imageio.ImageIO;
 
@@ -16,8 +9,18 @@ import java.awt.EventQueue;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -25,10 +28,107 @@ import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class Client {
+public class Client extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    private JLabel imageLabel;
+    private BufferedImage selectedImage;
+
+    public Client() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+
+        JPanel buttonPanel = new JPanel();
+        contentPane.add(buttonPanel, BorderLayout.NORTH);
+
+        JButton btnSelectImage = new JButton("Select Image");
+        buttonPanel.add(btnSelectImage);
+
+        imageLabel = new JLabel("");
+        contentPane.add(imageLabel, BorderLayout.CENTER);
+
+        JButton btnSendImage = new JButton("Send Image");
+        buttonPanel.add(btnSendImage);
+
+        btnSelectImage.addActionListener(e -> selectImage());
+        btnSendImage.addActionListener(e -> sendImage());
+    }
+
+    private void selectImage() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Images", "jpg", "jpeg", "png", "gif"));
+        int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try {
+                selectedImage = ImageIO.read(fileChooser.getSelectedFile());
+                imageLabel.setIcon(new ImageIcon(selectedImage.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendImage() {
+        if (selectedImage != null) {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(selectedImage, "jpg", baos);
+                baos.flush();
+                byte[] bytes = baos.toByteArray();
+                baos.close();
+
+                Socket socket = new Socket("localhost", Server.getPort());
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                out.writeObject(bytes);
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                Client frame = new Client();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
+
+
+/*public class Client {
     private JFrame frame;
     private Image imageToSend;
     private Socket socket;
@@ -99,6 +199,6 @@ public class Client {
             }
         });
     }
-}
+}*/
 
 
--- src/uppgift214/Server.java
+++ src/uppgift214/Server.java
@@ -5,20 +5,154 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class Server extends JFrame {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-    private JLabel imageLabel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Server {
+
+    private static final int PORT = 4848;
+
+    private List<ObjectOutputStream> outputStreams = new ArrayList<>();
+    private JTextArea logArea;
+
+    public static int getPort() {
+        return PORT;
+    }
+
+    public void start() {
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new JFrame("Server");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            logArea = new JTextArea();
+            JScrollPane scrollPane = new JScrollPane(logArea);
+            scrollPane.setPreferredSize(new Dimension(500, 300));
+            frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+
+            frame.pack();
+            frame.setVisible(true);
+        });
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            log("Server started on port " + PORT);
+
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                log("New client connected: " + clientSocket.getInetAddress());
+
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                outputStreams.add(out);
+
+                ClientHandler clientHandler = new ClientHandler(clientSocket, out);
+                new Thread(clientHandler).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private class ClientHandler implements Runnable {
+        private Socket socket;
+        private ObjectInputStream in;
+        private ObjectOutputStream out;
+
+        public ClientHandler(Socket socket, ObjectOutputStream out) {
+            this.socket = socket;
+            this.out = out;
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    in = new ObjectInputStream(socket.getInputStream());
+                    Object obj = in.readObject();
+                    if (obj instanceof byte[]) {
+                        byte[] bytes = (byte[]) obj;
+                        log("Received image from client: " + bytes.length + " bytes");
+                        broadcast(bytes);
+                    }
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    out.close();
+                    outputStreams.remove(out);
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void broadcast(byte[] bytes) {
+            for (ObjectOutputStream stream : outputStreams) {
+                try {
+                    stream.writeObject(bytes);
+                    stream.reset();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void log(String message) {
+        EventQueue.invokeLater(() -> {
+            logArea.append(message + "\n");
+            logArea.setCaretPosition(logArea.getDocument().getLength());
+        });
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
+    }
+}
+
+/*public class Server extends JFrame implements Serializable {
+
+    /*private JLabel imageLabel;
 
     public Server() {
         super("Image Receiver");
@@ -41,9 +175,10 @@ public class Server extends JFrame {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         Server receiver = new Server();
-        receiver.receiveImage(8000);
+        receiver.receiveImage(5000);
     }
-}
+}*/
+
 
 
 
