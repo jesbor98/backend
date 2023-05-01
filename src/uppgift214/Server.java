00b81cbcd4641c3a3bac95d9d2ba41ab3c8a0ea2
jesbor98 <jessicalborg@gmail.com>
2023-05-01 14:02:24 +0200

    DONE! pic received OK-msg from Server when image sent

--- src/uppgift214/Server.java
+++ src/uppgift214/Server.java
@@ -1,49 +1,17 @@
 package uppgift214;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
-import javax.imageio.ImageIO;
-import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.ByteArrayInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.EventQueue;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import java.io.ObjectOutputStream;
 
 public class Server {
 
@@ -51,6 +19,7 @@ public class Server {
 
     private List<ObjectOutputStream> outputStreams = new ArrayList<>();
     private JTextArea logArea;
+    private JLabel imageLabel;
 
     public static int getPort() {
         return PORT;
@@ -60,10 +29,12 @@ public class Server {
         EventQueue.invokeLater(() -> {
             JFrame frame = new JFrame("Server");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+            imageLabel = new JLabel();
+            frame.getContentPane().add(imageLabel, BorderLayout.SOUTH);
             logArea = new JTextArea();
             JScrollPane scrollPane = new JScrollPane(logArea);
             scrollPane.setPreferredSize(new Dimension(500, 300));
+
             frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
 
             frame.pack();
@@ -125,16 +96,30 @@ public class Server {
             }
         }
 
+
+        // Lägg till följande metod i Server-klassen:
+        private void displayImage(byte[] bytes) {
+            try {
+                BufferedImage image = ImageIO.read(new ByteArrayInputStream(bytes));
+                imageLabel.setIcon(new ImageIcon(image.getScaledInstance(imageLabel.getWidth(), imageLabel.getHeight(), Image.SCALE_SMOOTH)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
         private void broadcast(byte[] bytes) {
             for (ObjectOutputStream stream : outputStreams) {
                 try {
                     stream.writeObject(bytes);
                     stream.reset();
+                    displayImage(bytes); // visa bilden på serverns skärm
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
+
     }
 
     private void log(String message) {
@@ -149,36 +134,6 @@ public class Server {
         server.start();
     }
 }
-
-/*public class Server extends JFrame implements Serializable {
-
-    /*private JLabel imageLabel;
-
-    public Server() {
-        super("Image Receiver");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 400);
-        imageLabel = new JLabel();
-        add(imageLabel);
-        setVisible(true);
-    }
-
-    public void receiveImage(int port) throws IOException, ClassNotFoundException {
-        ServerSocket serverSocket = new ServerSocket(port);
-        Socket socket = serverSocket.accept();
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-        while (true) {
-            BufferedImage image = ImageIO.read(in);
-            imageLabel.setIcon(new ImageIcon(image));
-        }
-    }
-
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Server receiver = new Server();
-        receiver.receiveImage(5000);
-    }
-}*/
-
 
 
 
