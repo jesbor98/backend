93dbec56dac7e6ac0be28409590966946e3ad9ee
jesbor98 <jessicalborg@gmail.com>
2023-05-01 16:22:23 +0200

    DONE!

--- src/uppgift311/SimpleBrowser.java
+++ src/uppgift311/SimpleBrowser.java
@@ -1,24 +1,27 @@
 package uppgift311;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 
 public class SimpleBrowser {
-    public static void main(String[] args) throws Exception {
-        System.out.print("Enter URL: ");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String urlString = reader.readLine();
 
+    public SimpleBrowser(String urlString) throws IOException {
         URL url = new URL(urlString);
         BufferedReader urlReader = new BufferedReader(new InputStreamReader(url.openStream()));
-
         String line = "";
         while ((line = urlReader.readLine()) != null) {
             System.out.println(line);
         }
-
         urlReader.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("Enter URL: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String urlString = reader.readLine();
+        SimpleBrowser simpleBrowser = new SimpleBrowser(urlString);
     }
 }
 
